74143904bfb68a166bb14236c5faf33d84310d2b
Ben Funnell <rzzmttzz@gmail.com>
2011-10-24 23:08:26 +1100

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdvancedTimer::Application.config.secret_token = '84a2917d181c05df1c5d8bb55ee0898d77be06089f1ff9db59722959263aa2e89d35cb1b80fe543f4264b1a8fb7f06a6fbdca430822ce53caa29cb79edaf2ec7'
